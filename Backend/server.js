6f8a75eff150ada93ff537b86a3004eaf20fd482
project-3 <your-github-pruthvirajul2001@gmail.com@example.com>
2025-06-26 17:02:05 +0530

    changes

--- Backend/server.js
+++ Backend/server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const { Pool } = require('pg');
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = 3129;
 
 // PostgreSQL connection configuration
 const pool = new Pool({
     user: 'postgres',
-    host: 'localhost',
+    host: 'postgres',
     database: 'jobflow_db',
-    password: 'root',
+    password: 'admin321',
     port: 5432,
 });
 
@@ -158,5 +158,5 @@ app.post('/api/favorites', async (req, res) => {
 
 // Start server
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server running at http://51.21.196.168:${port}`);
 });
